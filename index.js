85d4b94eb1233550508671e5697ff1f6c7f1ea1c
Rest <code.rest.dev@gmail.com>
2024-02-15 23:52:19 -0300

    0.0.4 http methods en express

--- index.js
+++ index.js
@@ -15,25 +15,52 @@ const express = require("express")
 
 const app = express()
 
+
+app.get("/products", (req, res)=> {
+    res.send("lista de productos")
+})
+
+app.post("/products", (req, res)=> {
+    res.send("creando productos")
+})
+
+app.put("/products", (req, res)=> {
+    res.send("Actualizando producto")
+})
+
+app.delete("/products", (req, res)=> {
+    res.send("Borrando producto")
+})
+
+app.patch("/products", (req, res)=> {
+    res.send("Actualizando una parte del productos")
+})
+
 app.listen(3000)
-
-app.get("/", (req, res)=>{
-    res.send("hello wrld")
-})
-
-app.get("/about", (req, res)=>{
-    res.send("About")
-})
-
-
-app.get("/weather", (req, res)=>{
-    res.send("The current wather is Nice")
-})
-
-app.use((req,res)=>{
-    res.status(404).send("No se encontro tu pagina")
-})
-
-
-
 console.log("server on port 3000")
+
+
+
+
+
+
+
+// app.get("/", (req, res)=>{
+//     res.send("hello wrld")
+// })
+
+// app.get("/about", (req, res)=>{
+//     res.send("About")
+// })
+
+
+// app.get("/weather", (req, res)=>{
+//     res.send("The current wather is Nice")
+// })
+
+// app.use((req,res)=>{
+//     res.status(404).send("No se encontro tu pagina")
+// })
+
+
+

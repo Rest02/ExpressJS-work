d689e4226bcf3858a5918f7472ec003d274764c0
Rest <code.rest.dev@gmail.com>
2024-02-17 22:24:48 -0300

    0.0.6 Request Body

--- index.js
+++ index.js
@@ -13,42 +13,17 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
-app.get("/user", (req, res) => {
-  res.json({
-    name: "Fazt",
-    lastname: "ray",
-    age: 40,
-    points: [1, 2, 3],
-    address: {
-      city: "new york",
-      street: "some street 123",
-    },
-  });
-});
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-app.get("/isAlive", (req, res) => {
-    res.sendStatus(200)
-  });
+
+app.post("/user", (req, res)=>{
+  console.log(req.body)
+  res.send("Nuevo usuario creado")
+})
 
 app.listen(3000);
 console.log("server on port 3000");
 
-// app.get("/", (req, res)=>{
-//     res.send("hello wrld")
-// })
-
-// app.get("/about", (req, res)=>{
-//     res.send("About")
-// })
-
-// app.get("/weather", (req, res)=>{
-//     res.send("The current wather is Nice")
-// })
-
-// app.use((req,res)=>{
-//     res.status(404).send("No se encontro tu pagina")
-// })

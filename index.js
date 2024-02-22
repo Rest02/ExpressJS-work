3a48400ff4adf6de58702f6859ad7a5e48719cf9
Rest <code.rest.dev@gmail.com>
2024-02-22 20:01:34 -0300

    0.1.1 Introduccion a Middlewares II

--- index.js
+++ index.js
@@ -26,5 +26,21 @@ app.all("/about", (req , res)=> {
   res.send("about page")
 })
 
+
+
+
+app.use((req,res, next)=>{
+  if(req.query.login == "matias@gmail.com"){
+    next()
+  }else{
+    res.send("No autorizado")
+  }
+})
+
+app.get("/dashboard" , (req , res)=>{
+  res.send("Dashboard page")
+})
+
+
 app.listen(3000);
 console.log("server on port 3000");

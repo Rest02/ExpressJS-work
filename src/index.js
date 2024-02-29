8d9765acb9140e1cb753f522d3552c31d277f71b
Rest <code.rest.dev@gmail.com>
2024-02-28 23:24:21 -0300

    0.1.4 Static Files

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@
 
 const express = require("express");
 const morgan = require("morgan")
+const path = require("path")
 
 
 
@@ -22,9 +23,18 @@ app.set("case sensitive routing", true)
 app.set("appName", "Express Course")
 app.set("port", 3000)
 
+app.use(express.json())
 app.use(morgan("dev"))
 
-app.get("/profile", (req, res)=>{
+app.get("/node.txt" , (req, res) => {
+  res.send("este no es un archivo")
+})
+
+
+
+
+
+app.post("/profile", (req, res)=>{
   res.send("profile page")
 })
 
@@ -32,23 +42,23 @@ app.all("/about", (req , res)=> {
   res.send("about page")
 })
 
-app.all("/UserName", (req , res)=> {
-  res.send("Username page")
-})
 
-
-app.use((req,res, next)=>{
-  if(req.query.login == "matias@gmail.com"){
-    next()
-  }else{
-    res.send("No autorizado")
-  }
-})
+// app.use((req,res, next)=>{
+//   if(req.query.login == "matias@gmail.com"){
+//     next()
+//   }else{
+//     res.send("No autorizado")
+//   }
+// })
 
 app.get("/dashboard" , (req , res)=>{
-  res.send("Dashboard page")
+  res.sendFile("Dashboard page")
 })
 
+console.log(__dirname)
+
+app.use("/public" , express.static(path.join(__dirname ,"public")))
+app.use("/uploads" , express.static(path.join(__dirname, "uploads")))
 
 app.listen(app.get("port"));
 console.log(`server ${app.get("appName")} on port ${app.get("port")}`);

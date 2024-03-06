6e33bd6c81d32ba2343f49765027a36d1001f778
Rest <code.rest.dev@gmail.com>
2024-03-06 00:07:28 -0300

    0.2.1 Conexion a bases de datos parte 2

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const path = require("path");
 // const connectDB = require("./db")
 // require("ejs")
 
-
 // connectDB()
 
 // app.js
+const postgres = require("postgres");
+require("dotenv").config();
 
-const postgres = require('postgres');
-require('dotenv').config();
-
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+let {
+  PGHOST = "ep-red-rice-a5zv1g82.us-east-2.aws.neon.tech",
+  PGDATABASE = "expressdb",
+  PGUSER="Rest02",
+  PGPASSWORD="5WzPRdelj1YL",
+  ENDPOINT_ID="ep-red-rice-a5zv1g82",
+} = process.env;
 
 const sql = postgres({
   host: PGHOST,
@@ -20,38 +24,36 @@ const sql = postgres({
   username: PGUSER,
   password: PGPASSWORD,
   port: 5432,
-  ssl: 'require',
+  ssl: "require",
   connection: {
     options: `project=${ENDPOINT_ID}`,
   },
 });
 
 async function getPgVersion() {
-  const result = await sql`select version()`;
+  const result = await sql`select 1+1 as result`;
   console.log(result);
 }
 
 getPgVersion();
-
 // }}}}}}}}}}}}}}}}}}}}}}}}}
-
 
 const app = express();
 
-const HomeRoutes = require("./routes/home")
-const UserRoutes = require("./routes/users")
+const HomeRoutes = require("./routes/home");
+const UserRoutes = require("./routes/users");
 
 app.set("case sensitive routing", true);
 app.set("appName", "Express Course");
 app.set("port", 3000);
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(HomeRoutes)
-app.use(UserRoutes)
+app.use(HomeRoutes);
+app.use(UserRoutes);
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
